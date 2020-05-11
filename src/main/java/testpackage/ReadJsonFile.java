6e4a06892ef94857746b92b4764a769b6d30c5a5
Tchiring Lama <tchiringlama@Tchirings-MacBook-Air.local>
2020-05-11 14:27:43 +0545

    Sink Twitter Data to Postgres

--- src/main/java/testpackage/ReadJsonFile.java
+++ src/main/java/testpackage/ReadJsonFile.java
@@ -1,18 +1,18 @@
-package testpackage;
-
-
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-
-import java.io.FileReader;
-import java.io.IOException;
-
-public class ReadJsonFile {
-    public JsonObject fileJson() throws IOException {
-        JsonParser jsonParser = new JsonParser();
-        JsonElement jsonObject = jsonParser.parse(new FileReader("/Users/tchiringlama/tweets3"));
-//        JsonElement jsonObject = jsonParser.parse(new FileReader("/Users/tchiringlama/tweet2"));
-        return (JsonObject) jsonObject;
-    }
-}
+//package testpackage;
+//
+//
+//import com.google.gson.JsonElement;
+//import com.google.gson.JsonObject;
+//import com.google.gson.JsonParser;
+//
+//import java.io.FileReader;
+//import java.io.IOException;
+//
+//public class ReadJsonFile {
+//    public JsonObject fileJson() throws IOException {
+//        JsonParser jsonParser = new JsonParser();
+//        JsonElement jsonObject = jsonParser.parse(new FileReader("/Users/tchiringlama/tweets3"));
+////        JsonElement jsonObject = jsonParser.parse(new FileReader("/Users/tchiringlama/tweet2"));
+//        return (JsonObject) jsonObject;
+//    }
+//}
