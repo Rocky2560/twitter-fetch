e482c512d3795764974866359cc8ad5561b0a064
Tchiring Lama <tchiringlama@Tchirings-MacBook-Air.local>
2020-05-15 14:53:07 +0545

    Location Cleaning

--- src/main/scala/spark/ExplodeInsert.scala
+++ src/main/scala/spark/ExplodeInsert.scala
@@ -357,118 +357,118 @@ class ExplodeInsert {
 //  }
 //}
 
-object TestLocation {
-
-
-  def main(args: Array[String]): Unit = {
-    val read_file = new ReadJsonFile
-    val str_json = read_file.fileJson()
-    val cdf = new CreateDF
-    val location1 = cdf.json_to_df(str_json.toString: String)
-
-    val sp: CreateSparkConnection = new CreateSparkConnection;
-
-    val city_df1 = sp.spark.read.format("csv").option("header", "true").load("/Users/tchiringlama/nepali_cities.csv")
-    //    val city_df = city_df.apply(lambda x: x.astype(str).str.lower())
-    val city_df = city_df1.select(city_df1.columns.map(c => lower(col(c)).alias(c)): _*)
-
-    val countries_list = List("united arab emirates", "nigeria", "ghana", "pitcairn islands", "ethiopia", "algeria", "niue",
-      "jordan", "netherlands", "andorra", "turkey", "madagascar", "samoa", "turkmenistan",
-      "eritrea",
-      "paraguay", "greece", "cook islands", "iraq", "azerbaijan", "mali", "brunei", "thailand",
-      "central african republic", "gambia", "saint kitts and nevis", "china", "lebanon", "serbia",
-      "belize", "germany", "switzerland", "kyrgyzstan", "guinea-bissau", "colombia", "brazil",
-      "slovakia", "republic of the congo", "barbados", "belgium", "romania", "hungary", "argentina",
-      "egypt", "australia", "venezuela", "saint lucia", "united states virgin islands", "moldova",
-      "turks and caicos islands", "guinea", "denmark", "senegal", "syria", "bangladesh",
-      "east timor",
-      "djibouti", "tanzania", "qatar", "isle of man", "ireland", "tajikistan", "tristan da cunha",
-      "sahrawi arab democratic republic [c]", "akrotiri and dhekelia", "christmas island",
-      "sierra leone", "tuvalu", "botswana", "cayman islands", "ascension island", "guyana",
-      "gibraltar",
-      "burundi", "guatemala", "saint barthélemy", "guam", "bermuda", "easter island", "vietnam",
-      "zimbabwe", "somaliland", "cuba", "finland", "solomon islands", "pakistan", "indonesia",
-      "saint helena", "syrian opposition", "israel (de facto)  palestine (claimed)", "south sudan",
-      "afghanistan", "uganda", "nepal", "sudan", "ukraine", "rwanda",
-      "south georgia and the south sandwich islands", "jamaica", "norfolk island",
-      "saint vincent and the grenadines", "democratic republic of the congo", "malaysia", "kuwait",
-      "gabon", "malawi", "peru", "portugal", "slovenia", "togo", "united kingdom", "angola",
-      "zambia",
-      "luxembourg", "spain", "marshall islands", "equatorial guinea", "maldives", "nicaragua",
-      "bahrain", "philippines", "mozambique", "saint martin", "lesotho", "wallis and futuna",
-      "eswatini (swaziland)", "mexico", "belarus", "somalia", "monaco", "liberia", "uruguay",
-      "comoros",
-      "russia", "oman", "kenya", "bahamas", "myanmar", "chad", "india", "palau", "niger", "cyprus",
-      "northern cyprus", "mauritania", "new caledonia", "tonga", "kazakhstan", "greenland", "aruba",
-      "norway", "canada", "burkina faso", "american samoa", "federated states of micronesia",
-      "panama",
-      "french polynesia", "suriname", "france", "sint maarten", "cambodia", "montserrat",
-      "montenegro",
-      "mauritius", "papua new guinea", "vanuatu", "haiti", "trinidad and tobago", "benin",
-      "czech republic", "cape verde", "south africa", "kosovo[g]", "north korea", "ecuador",
-      "morocco",
-      "palestine", "iceland", "latvia", "saudi arabia", "british virgin islands", "italy",
-      "dominica",
-      "northern mariana islands", "costa rica", "puerto rico", "san marino", "el salvador", "yemen",
-      "chile", "dominican republic", "são tomé and príncipe", "bosnia and herzegovina",
-      "south korea",
-      "singapore", "north macedonia", "bulgaria", "sri lanka", "grenada", "jersey",
-      "antigua and barbuda", "guernsey", "saint pierre and miquelon", "falkland islands", "artsakh",
-      "sweden", "bolivia", "abkhazia", "fiji", "taiwan", "estonia", "kiribati", "uzbekistan",
-      "georgia",
-      "honduras", "iran", "bhutan", "albania", "transnistria", "japan", "faroe islands", "libya",
-      "south ossetia", "tunisia", "mongolia", "liechtenstein", "malta", "anguilla", "vatican city",
-      "seychelles", "austria", "laos", "lithuania", "poland", "united states", "new zealand",
-      "cocos (keeling) islands", "curaçao", "namibia", "ivory coast", "cameroon", "nauru",
-      "armenia",
-      "croatia")
-
-
-    val cities_list = city_df.select(col("Name")).collect().map(_ (0)).toSet
-    val other_list1 = city_df.select(col("District")).collect().map(_ (0)).toSet
-    val province = city_df.select(col("Province")).collect().map(_ (0)).toSet
-    val other_list2 = other_list1 ++ province
-    val other_list = other_list2 ++ countries_list
-
-    import sp.spark.implicits._
-
-    val location = location1.select("user.location")
-    val check_null:String = location.select("location").as("String").collectAsList().get(0).toString
-    println(check_null)
-
-    if (check_null == "[null]" || check_null == " ") {
-      println("NEPAL")
-    } else {
-      val newDf = location.withColumn("location", regexp_replace(location("location"), """[^ \p{L} ^ \w]""", ""))
-      val df = newDf.withColumn("location", regexp_replace(newDf("location"), """\s+""", " "))
-      //      df.show(false)
-      println("INSIDE DF")
-      df.select("location").show(false)
-
-      val loc1 = df.collectAsList().get(0)(0).toString.toLowerCase
-      val loc = loc1.split(" ")
-
-      val city_check = loc.intersect(cities_list.toList)
-      val other_check = loc.intersect(other_list.toList)
-
-      if (city_check.nonEmpty) {
-        println("CITY CHECK = " + city_check(0).capitalize)
-        println("\n")
-        //        return city_check(0)
-      }
-      else if (other_check.nonEmpty) {
-        println("OTHER_CHECK = " + other_check(0).capitalize)
-        println("\n")
-        //        return other_check(0)
-      }
-      else {
-        if (loc1.isEmpty) {
-          println("TALA KO NEPAL")
-
-        } else {
-          println("LOCATION ELSE = " + loc1.capitalize)
-        }
-      }
-    }
-  }
-}
+//object TestLocation {
+//
+//
+//  def main(args: Array[String]): Unit = {
+//    val read_file = new ReadJsonFile
+//    val str_json = read_file.fileJson()
+//    val cdf = new CreateDF
+//    val location1 = cdf.json_to_df(str_json.toString: String)
+//
+//    val sp: CreateSparkConnection = new CreateSparkConnection;
+//
+//    val city_df1 = sp.spark.read.format("csv").option("header", "true").load("/Users/tchiringlama/nepali_cities.csv")
+//    //    val city_df = city_df.apply(lambda x: x.astype(str).str.lower())
+//    val city_df = city_df1.select(city_df1.columns.map(c => lower(col(c)).alias(c)): _*)
+//
+//    val countries_list = List("united arab emirates", "nigeria", "ghana", "pitcairn islands", "ethiopia", "algeria", "niue",
+//      "jordan", "netherlands", "andorra", "turkey", "madagascar", "samoa", "turkmenistan",
+//      "eritrea",
+//      "paraguay", "greece", "cook islands", "iraq", "azerbaijan", "mali", "brunei", "thailand",
+//      "central african republic", "gambia", "saint kitts and nevis", "china", "lebanon", "serbia",
+//      "belize", "germany", "switzerland", "kyrgyzstan", "guinea-bissau", "colombia", "brazil",
+//      "slovakia", "republic of the congo", "barbados", "belgium", "romania", "hungary", "argentina",
+//      "egypt", "australia", "venezuela", "saint lucia", "united states virgin islands", "moldova",
+//      "turks and caicos islands", "guinea", "denmark", "senegal", "syria", "bangladesh",
+//      "east timor",
+//      "djibouti", "tanzania", "qatar", "isle of man", "ireland", "tajikistan", "tristan da cunha",
+//      "sahrawi arab democratic republic [c]", "akrotiri and dhekelia", "christmas island",
+//      "sierra leone", "tuvalu", "botswana", "cayman islands", "ascension island", "guyana",
+//      "gibraltar",
+//      "burundi", "guatemala", "saint barthélemy", "guam", "bermuda", "easter island", "vietnam",
+//      "zimbabwe", "somaliland", "cuba", "finland", "solomon islands", "pakistan", "indonesia",
+//      "saint helena", "syrian opposition", "israel (de facto)  palestine (claimed)", "south sudan",
+//      "afghanistan", "uganda", "nepal", "sudan", "ukraine", "rwanda",
+//      "south georgia and the south sandwich islands", "jamaica", "norfolk island",
+//      "saint vincent and the grenadines", "democratic republic of the congo", "malaysia", "kuwait",
+//      "gabon", "malawi", "peru", "portugal", "slovenia", "togo", "united kingdom", "angola",
+//      "zambia",
+//      "luxembourg", "spain", "marshall islands", "equatorial guinea", "maldives", "nicaragua",
+//      "bahrain", "philippines", "mozambique", "saint martin", "lesotho", "wallis and futuna",
+//      "eswatini (swaziland)", "mexico", "belarus", "somalia", "monaco", "liberia", "uruguay",
+//      "comoros",
+//      "russia", "oman", "kenya", "bahamas", "myanmar", "chad", "india", "palau", "niger", "cyprus",
+//      "northern cyprus", "mauritania", "new caledonia", "tonga", "kazakhstan", "greenland", "aruba",
+//      "norway", "canada", "burkina faso", "american samoa", "federated states of micronesia",
+//      "panama",
+//      "french polynesia", "suriname", "france", "sint maarten", "cambodia", "montserrat",
+//      "montenegro",
+//      "mauritius", "papua new guinea", "vanuatu", "haiti", "trinidad and tobago", "benin",
+//      "czech republic", "cape verde", "south africa", "kosovo[g]", "north korea", "ecuador",
+//      "morocco",
+//      "palestine", "iceland", "latvia", "saudi arabia", "british virgin islands", "italy",
+//      "dominica",
+//      "northern mariana islands", "costa rica", "puerto rico", "san marino", "el salvador", "yemen",
+//      "chile", "dominican republic", "são tomé and príncipe", "bosnia and herzegovina",
+//      "south korea",
+//      "singapore", "north macedonia", "bulgaria", "sri lanka", "grenada", "jersey",
+//      "antigua and barbuda", "guernsey", "saint pierre and miquelon", "falkland islands", "artsakh",
+//      "sweden", "bolivia", "abkhazia", "fiji", "taiwan", "estonia", "kiribati", "uzbekistan",
+//      "georgia",
+//      "honduras", "iran", "bhutan", "albania", "transnistria", "japan", "faroe islands", "libya",
+//      "south ossetia", "tunisia", "mongolia", "liechtenstein", "malta", "anguilla", "vatican city",
+//      "seychelles", "austria", "laos", "lithuania", "poland", "united states", "new zealand",
+//      "cocos (keeling) islands", "curaçao", "namibia", "ivory coast", "cameroon", "nauru",
+//      "armenia",
+//      "croatia")
+//
+//
+//    val cities_list = city_df.select(col("Name")).collect().map(_ (0)).toSet
+//    val other_list1 = city_df.select(col("District")).collect().map(_ (0)).toSet
+//    val province = city_df.select(col("Province")).collect().map(_ (0)).toSet
+//    val other_list2 = other_list1 ++ province
+//    val other_list = other_list2 ++ countries_list
+//
+//    import sp.spark.implicits._
+//
+//    val location = location1.select("user.location")
+//    val check_null:String = location.select("location").as("String").collectAsList().get(0).toString
+//    println(check_null)
+//
+//    if (check_null == "[null]" || check_null == " ") {
+//      println("NEPAL")
+//    } else {
+//      val newDf = location.withColumn("location", regexp_replace(location("location"), """[^ \p{L} ^ \w]""", ""))
+//      val df = newDf.withColumn("location", regexp_replace(newDf("location"), """\s+""", " "))
+//      //      df.show(false)
+//      println("INSIDE DF")
+//      df.select("location").show(false)
+//
+//      val loc1 = df.collectAsList().get(0)(0).toString.toLowerCase
+//      val loc = loc1.split(" ")
+//
+//      val city_check = loc.intersect(cities_list.toList)
+//      val other_check = loc.intersect(other_list.toList)
+//
+//      if (city_check.nonEmpty) {
+//        println("CITY CHECK = " + city_check(0).capitalize)
+//        println("\n")
+//        //        return city_check(0)
+//      }
+//      else if (other_check.nonEmpty) {
+//        println("OTHER_CHECK = " + other_check(0).capitalize)
+//        println("\n")
+//        //        return other_check(0)
+//      }
+//      else {
+//        if (loc1.isEmpty) {
+//          println("TALA KO NEPAL")
+//
+//        } else {
+//          println("LOCATION ELSE = " + loc1.capitalize)
+//        }
+//      }
+//    }
+//  }
+//}
