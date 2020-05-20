a18c562411fa83522a0da35c80f7a0d1819452e0
Tchiring Lama <tchiringlama@Tchirings-MacBook-Air.local>
2020-05-20 17:58:26 +0545

    Created_at Data type change

--- src/main/scala/spark/ExplodeInsert.scala
+++ src/main/scala/spark/ExplodeInsert.scala
@@ -133,19 +133,22 @@ class ExplodeInsert {
           df.select(col("created_at"), col("id"), col("text"), col("source"), col("user.id").as("user_id"),
             col("in_reply_to_status_id_str").as("in_reply_to_status_id"), col("in_reply_to_user_id_str").as("in_reply_to_user_id"),
             col("lang"), col("retweet_count"), col("reply_count"),
-            col("entities.hashtags.text").as("hashtags"))
+            col("entities.hashtags.text").as("hashtags")).withColumn("created_at", to_timestamp(col("created_at"), "EEE MMM d HH:mm:ss z yyyy"))
+            .withColumn("created_at", to_utc_timestamp(col("created_at"), "Asia/Kathmandu"))
         }
         else if (check_entities_mentions.get(0)(0) != null && check_entities_hashtags.get(0)(0) == null) {
           df.select(col("created_at"), col("id"), col("text"), col("source"), col("user.id").as("user_id"),
             col("in_reply_to_status_id_str").as("in_reply_to_status_id"), col("in_reply_to_user_id_str").as("in_reply_to_user_id"),
             col("lang"), col("retweet_count"), col("reply_count"),
             col("entities.hashtags").as("hashtags"), col("entities.user_mentions.id").as("user_mentions_id"),
-            col("entities.user_mentions.name").as("user_mentions_name"))
+            col("entities.user_mentions.name").as("user_mentions_name")).withColumn("created_at", to_timestamp(col("created_at"), "EEE MMM d HH:mm:ss z yyyy"))
+            .withColumn("created_at", to_utc_timestamp(col("created_at"), "Asia/Kathmandu"))
         } else {
           df.select(col("created_at"), col("id"), col("text"), col("source"), col("user.id").as("user_id"),
             col("in_reply_to_status_id_str").as("in_reply_to_status_id"), col("in_reply_to_user_id_str").as("in_reply_to_user_id"),
             col("lang"), col("retweet_count"), col("reply_count"),
-            col("entities.hashtags").as("hashtags"))
+            col("entities.hashtags").as("hashtags")).withColumn("created_at", to_timestamp(col("created_at"), "EEE MMM d HH:mm:ss z yyyy"))
+            .withColumn("created_at", to_utc_timestamp(col("created_at"), "Asia/Kathmandu"))
         }
       }
       else {
@@ -153,7 +156,8 @@ class ExplodeInsert {
           col("in_reply_to_status_id_str").as("in_reply_to_status_id"), col("in_reply_to_user_id_str").as("in_reply_to_user_id"),
           col("lang"), col("retweet_count"), col("reply_count"),
           col("entities.hashtags.text").as("hashtags"), col("entities.user_mentions.id").as("user_mentions_id"),
-          col("entities.user_mentions.name").as("user_mentions_name"))
+          col("entities.user_mentions.name").as("user_mentions_name")).withColumn("created_at", to_timestamp(col("created_at"), "EEE MMM d HH:mm:ss z yyyy"))
+          .withColumn("created_at", to_utc_timestamp(col("created_at"), "Asia/Kathmandu"))
       }
     }
     tweets_df
